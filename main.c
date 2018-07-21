dc305e2eb3bea3ea307f8d6b7344049cadd2ce04
Master-Chaos <thomas_pointner@gmx.at>
2018-07-21 11:01:18 +0200

    Add comments and two display messages

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 
 int main()
 {
-    int inputvalues[] = {4,5,12};
-    int length = sizeof(inputvalues) / sizeof(int);
+    int inputvalues[] = {4,5,12}; //NUMBERS AS INPUT FOR CALCULATE THE MEDIAN
+    int length = sizeof(inputvalues) / sizeof(int); //CALCULATE THE AMOUNT OF INPUTVALUES (=LENGTH OF ARRAY)
     float count = 0;
     float result;
 
-    for (int i = 0; i < length; i++ )
+    for (int i = 0; i < length; i++ ) //THE LOOP COUNTS ALL VALUES OF THE ARRAY
     {
-        count = count + inputvalues[i];
+        count = count + inputvalues[i]; //ADD NEW POSITION OF THE ARRAY TO VALUE IN COUNT
     }
 
-    result = count/length;
-    printf("%.2f / %d = %.2f\n", count, length, result);
-
+    result = count/length; //NOW CALCULATE MEDIAN BY VALUE OF COUNTS DIVIDE BY AMOUNT OF INPUTVALUES (=LENGTH OF ARRAY)
+    printf("####################################\n"); //PRINT OUT SEPARATING LINE
+    printf("##### SIMPLE MEDIAN CALCULATOR #####\n"); //PRINT OUT TITLE
+    printf("####################################\n"); //PRINT OUT SEPARATING LINE
+    printf("%.2f / %d = %.2f\n", count, length, result); //PRINT OUT THE CALCULATION AND THE RESULT OF IT
 
     return 0;
 }
